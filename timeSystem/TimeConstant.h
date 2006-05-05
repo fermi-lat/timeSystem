e8ad1a27189e02d1250e5ab8e2a04dbd3341e6c3
peachey <>
2006-05-05 18:57:45 +0000

    Add full complement of conversion factors (Hour to Sec etc).

--- timeSystem/TimeConstant.h
+++ timeSystem/TimeConstant.h
@@ -9,9 +9,29 @@
 namespace timeSystem {
 
   // Unit conversion constants.
-  double DayPerSec();
+  double MinPerSec();
+  double SecPerMin();
+  long SecPerMinLong();
 
+  double HourPerMin();
+  double MinPerHour();
+  long MinPerHourLong();
+
+  double DayPerHour();
+  double HourPerDay();
+  long HourPerDayLong();
+
+  double HourPerSec();
+  double SecPerHour();
+  long SecPerHourLong();
+
+  double DayPerMin();
+  double MinPerDay();
+  long MinPerDayLong();
+
+  double DayPerSec();
   double SecPerDay();
+  long SecPerDayLong();
 
   // Time system conversion constants.
   double TaiMinusTtSec();
@@ -21,12 +41,29 @@ namespace timeSystem {
   // Definitions.
 
   // Unit conversion constants.
+  inline double MinPerSec() { static double r = 1. / SecPerMin(); return r; }
+  inline double SecPerMin() { static double r = SecPerMinLong(); return r; }
+  inline long SecPerMinLong() { static long r = 60l; return r; }
+
+  inline double HourPerMin() { static double r = 1. / MinPerHour(); return r; }
+  inline double MinPerHour() { static double r = MinPerHourLong(); return r; }
+  inline long MinPerHourLong() { static long r = 60l; return r; }
+
+  inline double DayPerHour() { static double r = 1. / HourPerDay(); return r; }
+  inline double HourPerDay() { static double r = HourPerDayLong(); return r; }
+  inline long HourPerDayLong() { static long r = 24l; return r; }
+
+  inline double HourPerSec() { static double r = 1. / SecPerHour(); return r; }
+  inline double SecPerHour() { static double r = SecPerHourLong(); return r; }
+  inline long SecPerHourLong() { static long r = SecPerMinLong() * MinPerHourLong(); return r; }
+
+  inline double DayPerMin() { static double r = 1. / MinPerDay(); return r; }
+  inline double MinPerDay() { static double r = MinPerDayLong(); return r; }
+  inline long MinPerDayLong() { static long r = MinPerHourLong() * HourPerDayLong(); return r; }
+
   inline double DayPerSec() { static double r = 1. / SecPerDay(); return r; }
-
-  // TODO: use integral value?
-  inline double SecPerDay() { static double r = 86400.; return r; }
-
-  // TODO: add HourPerDay() etc.
+  inline double SecPerDay() { static double r = SecPerDayLong(); return r; }
+  inline long SecPerDayLong() { static long r = SecPerMinLong() * MinPerHourLong() * HourPerDayLong(); return r; }
 
   // Time system conversion constants.
   inline double TaiMinusTtSec() { static double r = -TtMinusTaiSec(); return r; }
