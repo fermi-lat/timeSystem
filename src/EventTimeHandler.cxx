715bc6d4f8a67d45fede35b8016ad6208526bf90
hirayama <>
2007-09-07 22:50:06 +0000

    Change to use BaryTimeComputer to compute barycentric times. Separate mission-specific part from EventTimeHandler to its subclasses.

--- src/EventTimeHandler.cxx
+++ src/EventTimeHandler.cxx
@@ -6,12 +6,9 @@
 #include "timeSystem/EventTimeHandler.h"
 
 #include "timeSystem/AbsoluteTime.h"
-#include "timeSystem/Duration.h"
-#include "timeSystem/ElapsedTime.h"
-#include "timeSystem/GlastMetRep.h"
-#include "timeSystem/IntFracPair.h"
-#include "timeSystem/TimeRep.h"
+#include "timeSystem/BaryTimeComputer.h"
 
+#include <cmath>
 #include <sstream>
 #include <stdexcept>
 
@@ -21,9 +18,9 @@ extern "C" {
 
 namespace timeSystem {
 
-  EventTimeHandler::EventTimeHandler(tip::Table & table, double position_tolerance): m_table(&table), m_bary_time(false),
-    m_position_tolerance(position_tolerance), m_ra_nom(0.), m_dec_nom(0.), m_speed_of_light(0.), m_solar_mass(0.),
-    m_sc_file(), m_mjd_tt(new MjdRep("TT", 0, 0.)) {
+  EventTimeHandler::EventTimeHandler(tip::Table & table, double position_tolerance):
+    m_table(&table), m_bary_time(false), m_position_tolerance(position_tolerance), m_ra_nom(0.), m_dec_nom(0.),
+    m_computer(BaryTimeComputer::getComputer()) {
     // Get table header.
     const tip::Header & header(m_table->getHeader());
 
@@ -59,55 +56,9 @@ namespace timeSystem {
 
     // Pre-compute three vector version of RA_NOM and DEC_NOM.
     computeThreeVector(m_ra_nom, m_dec_nom, m_vect_nom);
-
-    // Get time system from TIMESYS keyword.
-    std::string time_system;
-    header["TIMESYS"].get(time_system);
-    for (std::string::iterator itor = time_system.begin(); itor != time_system.end(); ++itor) *itor = std::toupper(*itor);
-
-    // Create TimeRep.
-    // TODO: Consider replacing TimeRep with this class.
-    m_time_rep = new GlastMetRep(time_system, 0.);
   }
 
-  EventTimeHandler::~EventTimeHandler() {
-    delete m_mjd_tt;
-    delete m_time_rep;
-  }
-
-  void EventTimeHandler::initialize(const std::string & pl_ephem, const std::string & sc_file) {
-    // Convert pl_ephem argument of this method to ephnum argument of initephem C-function.
-    int ephnum = 0;
-    std::string pl_ephem_uc(pl_ephem);
-    for (std::string::iterator itor = pl_ephem_uc.begin(); itor != pl_ephem_uc.end(); ++itor) *itor = std::toupper(*itor);
-    if ("JPL DE200" == pl_ephem_uc) ephnum = 200;
-    else if ("JPL DE405" == pl_ephem_uc) ephnum = 405;
-    else if ("AUTO" == pl_ephem_uc) ephnum = 0;
-    else throw std::runtime_error("Solar system ephemeris \"" + pl_ephem + "\" not supported.");
-
-    // Call initephem C-function.
-    int denum = 0;
-    double radsol = 0.;
-    int status = initephem(ephnum, &denum, &m_speed_of_light, &radsol, &m_solar_mass);
-
-    // Check initialization status.
-    if (status) {
-      std::ostringstream os;
-      os << "Error while initializing ephemeris (status = " << status << ").";
-      throw std::runtime_error(os.str());
-    }
-
-    // Set space craft file to internal variable.
-    m_sc_file = sc_file;
-    // TODO: Any better way than below?
-    m_sc_file_char = (char *)m_sc_file.c_str();
-
-    // Initialize clock and orbit
-    // TODO: Accept other missions?  What type (and name) of the argument?
-    enum Observatory mission(GLAST);
-    scorbitinit (mission);
-    clockinit (mission);
-  }
+  EventTimeHandler::~EventTimeHandler() {}
 
   AbsoluteTime EventTimeHandler::readHeader(const std::string & keyword_name) {
     return readHeader(keyword_name, false, 0., 0.);
@@ -125,30 +76,6 @@ namespace timeSystem {
       // Delegate computation of barycenteric time and return the result.
       return readHeader(keyword_name, true, ra, dec);
     }
-  }
-
-  AbsoluteTime EventTimeHandler::readHeader(const std::string & keyword_name, const bool request_bary_time,
-    const double ra, const double dec) {
-    // Read keyword value from header as a signle variable of double type.
-    const tip::Header & header(m_table->getHeader());
-    double keyword_value = 0.;
-    header[keyword_name].get(keyword_value);
-
-    // Convert keyword value to AbsoluteTime.
-    m_time_rep->set("TIME", keyword_value);
-    AbsoluteTime abs_time(*m_time_rep);
-
-    if (request_bary_time) {
-      // Get space craft position at the given time.
-      int error = 0;
-      double * sc_position = xscorbit(m_sc_file_char, keyword_value, &error);
-
-      // Perform barycentric correction on abs_time.
-      computeBaryTime(ra, dec, sc_position, abs_time);
-    }
-
-    // Return the requested time.
-    return abs_time;
   }
 
   AbsoluteTime EventTimeHandler::readColumn(const std::string & column_name) {
@@ -169,28 +96,6 @@ namespace timeSystem {
     }
   }
 
-  AbsoluteTime EventTimeHandler::readColumn(const std::string & column_name, const bool request_bary_time,
-    const double ra, const double dec) {
-    // Read column value from a given record as a signle variable of double type.
-    double column_value = (*m_record_itor)[column_name].get();
-
-    // Convert keyword value to AbsoluteTime.
-    m_time_rep->set("TIME", column_value);
-    AbsoluteTime abs_time(*m_time_rep);
-
-    if (request_bary_time) {
-      // Get space craft position at the given time.
-      int error = 0;
-      double * sc_position = xscorbit(m_sc_file_char, column_value, &error);
-
-      // Perform barycentric correction on abs_time.
-      computeBaryTime(ra, dec, sc_position, abs_time);
-    }
-
-    // Return the requested time.
-    return abs_time;
-  }
-
   void EventTimeHandler::setFirstRecord() {
     m_record_itor = m_table->begin();
   }
@@ -203,55 +108,16 @@ namespace timeSystem {
     return (m_record_itor == m_table->end());
   }
 
+  tip::Header & EventTimeHandler::getHeader() const {
+    return m_table->getHeader();
+  }
+
+  tip::TableRecord & EventTimeHandler::getCurrentRecord() const {
+    return *m_record_itor;
+  }
+
   void EventTimeHandler::computeBaryTime(const double ra, const double dec, const double sc_position[], AbsoluteTime & abs_time) const {
-    // Set given time to a variable to pass to dpleph C-function.
-    double jdt[2];
-    *m_mjd_tt = abs_time;
-    long mjd_int = 0;
-    double mjd_frac = 0.;
-    m_mjd_tt->get("MJDI", mjd_int);
-    m_mjd_tt->get("MJDF", mjd_frac);
-    // TODO: Implement JdRep and use it instead of MjdRep.
-    jdt[0] = mjd_int + 2400001;
-    jdt[1] = mjd_frac - 0.5;
-
-    // Read solar system ephemeris for the given time.
-    const int iearth = 3;
-    const int isun = 11;
-    const double *eposn;
-    eposn = dpleph(jdt, iearth, isun);
-    if (NULL == eposn) {
-      std::ostringstream os;
-      os << "Could not find solar system ephemeris for " << *m_mjd_tt << ".";
-      throw std::runtime_error(os.str());
-    }
-
-    // Compute vectors between related objects.
-    const double *rce, *rcs, *vce;
-    double rca[3], rsa[3];
-    rce = eposn;
-    vce = eposn + 3;
-    rcs = eposn + 6;
-    for (int idx = 0; idx < 3; ++idx) {
-      // Compute SSBC-to-S/C vector.
-      rca[idx] = rce[idx] + sc_position[idx]/m_speed_of_light;
-
-      // Compute Sun-to-S/C vector.
-      rsa[idx] = rca[idx] - rcs[idx];
-    }
-
-    // Convert source direction from (RA, Dec) to a three-vector.
-    double sourcedir[3];
-    computeThreeVector(ra, dec, sourcedir);
-
-    // Compute total propagation delay.
-    double sundis = sqrt(computeInnerProduct(rsa, rsa));
-    double cth = computeInnerProduct(sourcedir, rsa) / sundis;
-    double delay = computeInnerProduct(sourcedir, rca) + computeInnerProduct(sc_position, vce)/m_speed_of_light
-      + 2*m_solar_mass*log(1.+cth);
-
-    // Compute a barycenteric time for the give arrival time.
-    abs_time += ElapsedTime("TDB", Duration(IntFracPair(delay), Sec));
+    m_computer.computeBaryTime(ra, dec, sc_position, abs_time);
   }
 
   double EventTimeHandler::computeInnerProduct(const double vect_x[], const double vect_y[]) const {
