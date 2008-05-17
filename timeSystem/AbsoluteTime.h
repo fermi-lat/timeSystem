47b357b062b93d903ed39f0062d22b914d80ce55
hirayama <>
2008-05-17 00:15:05 +0000

    Add constructors, setters, getters, a parser, and a printer.

--- timeSystem/AbsoluteTime.h
+++ timeSystem/AbsoluteTime.h
@@ -21,6 +21,31 @@ namespace timeSystem {
   class TimeRep;
   class TimeSystem;
 
+  /** \class Mjd
+      \brief Class to hold an MJD number in two parts, the integer part and the fractional part, in order to
+             hold it in a precision required by GLAST.
+  */
+  struct Mjd {
+    Mjd(long int_part, double frac_part): m_int(int_part), m_frac(frac_part) {}
+    long m_int;
+    double m_frac;
+  };
+
+  /** \class Mjd1
+      \brief Class to hold an MJD number in one double-precision variable.
+  */
+  struct Mjd1 {
+    Mjd1(double value): m_value(value) {}
+    // TODO: Remove m_value to m_mjd or m_day?
+    // NOTE: Name m_mjd could be very confusing when JD is defined and uses m_jd.
+    double m_value;
+  };
+
+  // TODO: Implement struct Jd, Jd1, MonthDay, WeekDay, YearDay.
+  // NOTE: Data members of struct MonthDay: m_year, m_mon,  m_mday, m_hour, m_min, and m_sec (cf. struct tm in C).
+  // NOTE: Data members of struct WeekDay:  m_year, m_week, m_wday, m_hour, m_min, and m_sec (cf. struct tm in C).
+  // NOTE: Data members of struct YearDay:  m_year,         m_yday, m_hour, m_min, and m_sec (cf. struct tm in C).
+
   /** \class AbsoluteTime
       \brief Class which represents an absolute moment in time, expressed as a time elapsed from a specific MJD time, in
              a particular time unit in a particular time system. This class transparently handles all conversions between time
@@ -29,7 +54,24 @@ namespace timeSystem {
   */
   class AbsoluteTime {
     public:
+      // TODO: Remove constructor taking two Duration objects.
       AbsoluteTime(const std::string & time_system_name, const Duration & origin, const Duration & time);
+
+      AbsoluteTime(const std::string & time_system_name, long mjd_day, double mjd_sec);
+
+      AbsoluteTime(const std::string & time_system_name, const Mjd & mjd);
+      AbsoluteTime(const std::string & time_system_name, const Mjd1 & mjd);
+
+      void get(const std::string & time_system_name, Mjd & mjd) const;
+      void get(const std::string & time_system_name, Mjd1 & mjd) const;
+
+      void set(const std::string & time_system_name, const Mjd & mjd);
+      void set(const std::string & time_system_name, const Mjd1 & mjd);
+
+      void set(const std::string & time_system_name, const std::string & time_format_name, const std::string & time_string);
+
+      std::string represent(const std::string & time_system_name, const std::string & time_format_name,
+        std::streamsize precision = std::numeric_limits<double>::digits10) const;
 
       AbsoluteTime(const TimeRep & rep);
 
@@ -81,9 +123,8 @@ namespace timeSystem {
 
   template <typename StreamType>
   inline void AbsoluteTime::write(StreamType & os) const {
-    // "123 days 456.789 seconds since 54321.987 MJD (TDB)"
-    Moment this_time = convert(m_moment);
-    os << this_time.second << " since " << this_time.first << " MJD (" << *m_time_system << ")";
+    // Write the time in the format of "123.456789 seconds after 54321.0 MJD (TDB)".
+    os << m_moment.second << " seconds after " << m_moment.first << ".0 MJD (" << *m_time_system << ")";
   }
 
   std::ostream & operator <<(std::ostream & os, const AbsoluteTime & time);
