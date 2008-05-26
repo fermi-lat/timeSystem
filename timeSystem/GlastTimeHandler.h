918715389d1d91d0cd0cf4507de7c90d777d99d8
hirayama <>
2008-05-26 01:38:30 +0000

    Add a data member of Mjd type to hold MJDREF, and stop using MetRep objects. Adjust const-ness of methods to follow changes in the base class.

--- timeSystem/GlastTimeHandler.h
+++ timeSystem/GlastTimeHandler.h
@@ -6,6 +6,7 @@
 #ifndef timeSystem_GlastTimeHandler_h
 #define timeSystem_GlastTimeHandler_h
 
+#include "timeSystem/AbsoluteTime.h"
 #include "timeSystem/EventTimeHandler.h"
 
 #include <string>
@@ -18,9 +19,7 @@ namespace tip {
 
 namespace timeSystem {
 
-  class AbsoluteTime;
   class BaryTimeComputer;
-  class TimeRep;
 
   /** \class GlastTimeHandler
       \brief Class which reads out event times from a GLAST event file, creates AbsoluteTime objects for event times,
@@ -35,24 +34,25 @@ namespace timeSystem {
 
       virtual void setSpacecraftFile(const std::string & sc_file_name, const std::string & sc_extension_name);
 
-      virtual AbsoluteTime parseTimeString(const std::string & time_string, const std::string & time_system = "FILE");
+      virtual AbsoluteTime parseTimeString(const std::string & time_string, const std::string & time_system = "FILE") const;
 
     protected:
       virtual AbsoluteTime readTime(const tip::Header & header, const std::string & keyword_name, const bool request_bary_time,
-        const double ra, const double dec);
+        const double ra, const double dec) const;
 
       virtual AbsoluteTime readTime(const tip::TableRecord & record, const std::string & column_name, const bool request_bary_time,
-        const double ra, const double dec);
+        const double ra, const double dec) const;
 
     private:
       std::string m_time_system;
+      Mjd m_mjd_ref;
       std::string m_sc_file;
       char * m_sc_file_char;
 
       GlastTimeHandler(const std::string & file_name, const std::string & extension_name, const double angular_tolerance,
         const bool read_only = true);
 
-      AbsoluteTime computeAbsoluteTime(const double glast_time, const bool request_bary_time, const double ra, const double dec);
+      AbsoluteTime computeAbsoluteTime(const double glast_time, const bool request_bary_time, const double ra, const double dec) const;
 
       static bool checkHeaderKeyword(const std::string & file_name, const std::string & extension_name);
   };
