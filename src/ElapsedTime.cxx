131f3cd731a4a1444b9a10d26c7b33a93520c240
hirayama <>
2008-06-03 01:22:40 +0000

    Add getDuration methods (overloaded). Remove get/setTime methods. Rename some variables for clarity.

--- src/ElapsedTime.cxx
+++ src/ElapsedTime.cxx
@@ -9,29 +9,41 @@
 
 namespace timeSystem {
 
-  ElapsedTime::ElapsedTime(const std::string & time_system_name, const Duration & time):
-    m_time_system(&TimeSystem::getSystem(time_system_name)), m_time(time) {}
+  ElapsedTime::ElapsedTime(const std::string & time_system_name, const Duration & time_duration):
+    m_time_system(&TimeSystem::getSystem(time_system_name)), m_duration(time_duration) {}
 
-  ElapsedTime::ElapsedTime(const TimeSystem * time_system, const Duration & time):
-    m_time_system(time_system), m_time(time) {}
+  ElapsedTime::ElapsedTime(const TimeSystem * time_system, const Duration & time_duration):
+    m_time_system(time_system), m_duration(time_duration) {}
 
   AbsoluteTime ElapsedTime::operator +(const AbsoluteTime & absolute_time) const {
-    return absolute_time.computeAbsoluteTime(m_time_system->getName(), m_time);
+    return absolute_time.computeAbsoluteTime(m_time_system->getName(), m_duration);
   }
 
-  ElapsedTime ElapsedTime::operator -() const { return ElapsedTime(m_time_system, -m_time); }
+  ElapsedTime ElapsedTime::operator -() const { return ElapsedTime(m_time_system, -m_duration); }
 
-  Duration ElapsedTime::getTime() const { return m_time; }
+  const timeSystem::TimeSystem & ElapsedTime::getSystem() const { return *m_time_system; }
 
-  void ElapsedTime::setTime(const Duration & time) { m_time = time; }
+  void ElapsedTime::getDuration(const std::string & time_unit_name, long & time_value_int, double & time_value_frac) const {
+    m_duration.get(time_unit_name, time_value_int, time_value_frac);
+  }
 
-  std::ostream & operator <<(std::ostream & os, const ElapsedTime & time) {
-    time.write(os);
+  void ElapsedTime::getDuration(const std::string & time_unit_name, double & time_value) const {
+    m_duration.get(time_unit_name, time_value);
+  }
+
+  double ElapsedTime::getDuration(const std::string & time_unit_name) const {
+    return m_duration.get(time_unit_name);
+  }
+
+  Duration ElapsedTime::getDuration() const { return m_duration; }
+
+  std::ostream & operator <<(std::ostream & os, const ElapsedTime & elapsed_time) {
+    elapsed_time.write(os);
     return os;
   }
 
-  st_stream::OStream & operator <<(st_stream::OStream & os, const ElapsedTime & time) {
-    time.write(os);
+  st_stream::OStream & operator <<(st_stream::OStream & os, const ElapsedTime & elapsed_time) {
+    elapsed_time.write(os);
     return os;
   }
 
