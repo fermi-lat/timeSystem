8bcbd1d2089a59de725d5322bd7fd033f4a164d1
hirayama <>
2008-06-12 05:36:53 +0000

    Unify handling of integer overflows and underflows in a new method (round), and use it more consistently. Remove the constructor that takes a string. Remove default argument values from a constructor and add a constructor with no argument instead.

--- timeSystem/Duration.h
+++ timeSystem/Duration.h
@@ -18,23 +18,23 @@ namespace st_stream {
 namespace timeSystem {
 
   /** \class Duration
-      \brief Low level class used to represent an amount of time together with its nominal unit of measurement. Objects
-             of this type represent physical lengths of time only if used together with a time system.
+      \brief Low level class used to represent an amount of time duration together with its nominal unit of measurement.
+             Objects of this type represent physical lengths of time only if used together with a time system.
   */
   class Duration {
     public:
 
-      /** \brief Construct a duration object
-          \param t The amount of time.
-          \param unit The unit in which time t is measured.
+      /** \brief Construct a duration object from a pair of the numbers of days and seconds.
+          \param day The number of days.
+          \param sec The number of seconds.
       */
-      Duration(long day = 0, double sec = 0.): m_duration(add(duration_type(day, 0.), splitSec(sec))) {}
+      Duration(long day, double sec);
+
+      Duration();
 
       Duration(long time_value_int, double time_value_frac, const std::string & time_unit_name);
 
       Duration(double time_value, const std::string & time_unit_name);
-
-      Duration(const std::string & time_value, const std::string & time_unit_name);
 
       void get(const std::string & time_unit_name, long & time_value_int, double & time_value_frac) const;
 
@@ -76,27 +76,31 @@ namespace timeSystem {
     private:
       typedef std::pair<long, double> duration_type;
 
-      Duration(const duration_type & new_time): m_duration(new_time) {}
+      Duration(const duration_type & new_duration): m_duration(new_duration) {}
 
       /** \brief Convert any number of seconds into days & seconds in range [0, 86400).
           \param sec Input number of seconds.
       */
       duration_type splitSec(double sec) const;
 
-      /** \brief Add two times which are represented by long day and double second fields. Seconds
-            part of the result is guaranteed to be in the range [0., SecPerDay())
-          \param t1 The first time being added.
-          \param t2 The second time being added.
+      /** \brief Add two time durations which are represented by long day and double second fields. Seconds
+                 part of the result is guaranteed to be in the range [0., SecPerDay())
+          \param t1 The first time duration being added.
+          \param t2 The second time duration being added.
       */
       duration_type add(duration_type t1, duration_type t2) const;
 
-      /** \brief Multiply by -1 a time represented by long day and double second fields. Seconds
+      /** \brief Multiply by -1 a time duration represented by long day and double second fields. Seconds
             part of the result is guaranteed to be in the range [0., SecPerDay())
-          \param t1 The first time being negated.
+          \param t1 The first time duration being negated.
       */
       duration_type negate(duration_type t1) const;
 
       void set(long time_value_int, double time_value_frac, const std::string & time_unit_name);
+
+      long round(double value, const std::string & time_unit) const;
+
+      void convert(long day, double sec, duration_type & time_duration) const;
 
       duration_type m_duration;
   };
