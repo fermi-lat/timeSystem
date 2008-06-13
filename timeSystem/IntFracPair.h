185c0dd371002a7395b488e1d24c69d49d1eae84
hirayama <>
2008-06-13 05:26:27 +0000

    Add convert method to eliminate duplicated code. Modify a constructor to check a given fractional part is in the right range.

--- timeSystem/IntFracPair.h
+++ timeSystem/IntFracPair.h
@@ -16,8 +16,7 @@ namespace timeSystem {
     public:
       IntFracPair(): m_int_part(0), m_frac_part(0.) {}
 
-      // TODO: What if frac_part is out of [0., 1.) range?
-      IntFracPair(long int_part, double frac_part): m_int_part(int_part), m_frac_part(frac_part) {}
+      IntFracPair(long int_part, double frac_part);
 
       IntFracPair(double value);
 
@@ -33,6 +32,8 @@ namespace timeSystem {
       void write(StreamType & os) const;
 
     private:
+      long convert(double value) const;
+
       long m_int_part;
       double m_frac_part;
   };
