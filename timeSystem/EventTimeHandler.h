1d4622b5939c6026e7655532dfae87fc3530cddc
hirayama <>
2008-08-10 02:34:32 +0000

    Restructure EventTimeHandler classes to avoid unnecessary reading of spacecraft files.

--- timeSystem/EventTimeHandler.h
+++ timeSystem/EventTimeHandler.h
@@ -37,14 +37,13 @@ namespace timeSystem {
 
       void deregisterHandler();
 
-      static EventTimeHandler * createHandler(const std::string & file_name, const std::string & extension_name,
-        double angular_tolerance, bool read_only = true);
+      static EventTimeHandler * createHandler(const std::string & file_name, const std::string & extension_name, bool read_only = true);
 
     private:
       static cont_type & getFactoryContainer();
 
       virtual EventTimeHandler * createInstance(const std::string & file_name, const std::string & extension_name,
-        double angular_tolerance, bool read_only = true) const = 0;
+        bool read_only = true) const = 0;
   };
 
   /** \class IEventTimeHandlerFactory
@@ -54,8 +53,8 @@ namespace timeSystem {
   class EventTimeHandlerFactory: public IEventTimeHandlerFactory {
     private:
       virtual EventTimeHandler * createInstance(const std::string & file_name, const std::string & extension_name,
-        double angular_tolerance, bool read_only = true) const {
-        return HandlerType::createInstance(file_name, extension_name, angular_tolerance, read_only);
+        bool read_only = true) const {
+        return HandlerType::createInstance(file_name, extension_name, read_only);
       }
   };
 
@@ -67,20 +66,18 @@ namespace timeSystem {
     public:
       virtual ~EventTimeHandler();
 
-      static EventTimeHandler * createInstance(const std::string & file_name, const std::string & extension_name,
-        double angular_tolerance, bool read_only = true);
+      static EventTimeHandler * createInstance(const std::string & file_name, const std::string & extension_name, bool read_only = true);
 
-      virtual void setSpacecraftFile(const std::string & sc_file_name, const std::string & sc_extension_name) = 0;
+      virtual void initTimeCorrection(const std::string & sc_file_name, const std::string & sc_extension_name, 
+        const std::string & solar_eph, bool match_solar_eph, double angular_tolerance) = 0;
+
+      virtual void setSourcePosition(double ra, double dec) = 0;
+
+      virtual AbsoluteTime readTime(const std::string & field_name, bool from_header = false) const = 0;
+
+      virtual AbsoluteTime getBaryTime(const std::string & field_name, bool from_header = false) const = 0;
 
       virtual AbsoluteTime parseTimeString(const std::string & time_string, const std::string & time_system = "FILE") const = 0;
-
-      AbsoluteTime readHeader(const std::string & keyword_name) const;
-
-      AbsoluteTime readHeader(const std::string & keyword_name, double ra, double dec) const;
-
-      AbsoluteTime readColumn(const std::string & column_name) const;
-
-      AbsoluteTime readColumn(const std::string & column_name, double ra, double dec) const;
 
       void setFirstRecord();
 
@@ -96,44 +93,14 @@ namespace timeSystem {
 
       tip::TableRecord & getCurrentRecord() const;
 
-      void checkSkyPosition(double ra, double dec) const;
-
-      void checkSolarEph(const std::string & solar_eph) const;
-
     protected:
-      EventTimeHandler(const std::string & file_name, const std::string & extension_name, double angular_tolerance,
-        bool read_only = true);
-
-      virtual AbsoluteTime readTime(const tip::Header & header, const std::string & keyword_name, bool request_bary_time,
-        double ra, double dec) const = 0;
-
-      virtual AbsoluteTime readTime(const tip::TableRecord & record, const std::string & column_name, bool request_bary_time,
-        double ra, double dec) const = 0;
+      EventTimeHandler(const std::string & file_name, const std::string & extension_name, bool read_only = true);
 
       Mjd readMjdRef(const tip::Header & header) const;
 
-      void computeBaryTime(double ra, double dec, const std::vector<double> & sc_position, AbsoluteTime & abs_time)
-        const;
-
     private:
-      // Variables for event table handling.
-      std::string m_file_name;
-      std::string m_ext_name;
       tip::Table * m_table;
       tip::Table::Iterator m_record_itor;
-      bool m_bary_time;
-      double m_ra_nom;
-      double m_dec_nom;
-      std::vector<double> m_vect_nom; // Three vector representation of m_ra_nom and m_dec_nom.
-      double m_max_vect_diff;
-      std::string m_pl_ephem;
-
-      // Variables for barycentering.
-      BaryTimeComputer & m_computer;
-
-      double computeInnerProduct(const std::vector<double> & vect_x, const std::vector<double> & vect_y) const;
-
-      std::vector<double> computeThreeVector(double ra, double dec) const;
   };
 }
 
