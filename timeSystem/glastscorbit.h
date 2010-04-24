0fc8aa6ccac93110c96839339a35cf87bdd0f72a
hirayama <>
2010-04-24 04:06:56 +0000

    Enrich comments.

--- timeSystem/glastscorbit.h
+++ timeSystem/glastscorbit.h
@@ -8,26 +8,31 @@
 
 #include <fitsio.h>
 
+/* Error code to return if a given time is not covered by this spacecraft file */
+/* Note: The value must be different from any of the existing FITS error codes. */
 #define TIME_OUT_BOUNDS -2
 
-/* Structure to hold the spacecraft file information. */
+/* Structure to hold information of an opened spacecraft file */
 typedef struct {
-  fitsfile * fits_ptr;
-  long num_rows;
-  int colnum_scposn;
-  double * sctime_array;
-  long sctime_array_size;
-  char * filename;
-  char * extname;
-  int open_count;
+  fitsfile * fits_ptr;    /* Pointer to an opened spacecraft file */
+  long num_rows;          /* The number of rows in the file */
+  int colnum_scposn;      /* Column number of "SC_POSITION" column */
+  double * sctime_array;  /* Copy of the "START" column contents */
+  long sctime_array_size; /* Size of the above array */
+  char * filename;        /* Name of the opened spacecraft file */
+  char * extname;         /* Name of the spacecraft data extension */
+  int open_count;         /* The number of requests to open this file */
 } GlastScData;
 
+/* Structure to be given to the requester to open a spacecraft file */
+/* Note: A pointer to this structure is used as an identifier of an
+   opened spacecraft file. */
 typedef struct {
-  GlastScData ** data;
-  int status;
+  GlastScData ** data; /* Pointer to an internal spacecraft data table */
+  int status;          /* File I/O status (0 if normal) */
 } GlastScFile;
 
-// Declare function prototypes for GLAST spacecraft file access.
+/* Function prototypes for GLAST spacecraft file access */
 GlastScFile * glastscorbit_open(char *, char *);
 int glastscorbit_calcpos(GlastScFile *, double, double []);
 int glastscorbit_close(GlastScFile *);
